cca64aeb95acf004a335bf12b7e59b813e70a382
Ezequiel <ezequieltelekino@gmail.com>
2023-05-30 15:15:47 -0300

    Agrego lógica de getCart y getCartByID

--- primeraEntrega/src/routes/carts.router.js
+++ primeraEntrega/src/routes/carts.router.js
@@ -4,18 +4,27 @@ import {Product} from '../Product.js';
 
 
 const router = Router();
-let carritos = new CartManager("carritos.json")
+let cm = new CartManager("carritos.json")
 
 router.get("/", (req,res) => {
-    res.send(carritos.getCarts())
+    res.send(cm.getCarts())
 }) 
 
+router.get("/:cid", (req,res) => {
+    let id = req.params.cid
+    let devolver = cm.getCartByID(id)
+    if (!devolver){
+        res.status(404).send("Carrito no encontrado")
+        return
+    }
+    res.send(devolver)
+}) 
 
 router.post("/", (req, res) => {
-    carritos.carts = carritos.getCarts()
-    let carrito = carritos.addCart()
+    cm.carts = cm.getCarts()
+    let carrito = cm.addCart()
     if (carrito)
-        res.status(201).send(" Carrito agregado correctamente")
+        res.status(201).send(" Carrito " + carrito +" agregado correctamente")
     else
         res.status(404).send(" Error al agregar carrito: No se me ocurre cuál pueda llegar a ser el error.")
 })
